3277942efc07d6da264041dcd72ddcb23134c63a
JohnyHCL <44863546+JohnyHCL@users.noreply.github.com>
2019-01-11 07:36:26 +0100

    Update 1.5
    Added new function to KB button. Now it not only opens a new window with KB but also checks if the incident needs to be transfered, if yes, then i pastes the "Transferring to RG" into the worknotes field

--- ServiceNowHelper.user.js
+++ ServiceNowHelper.user.js
@@ -1,20 +1,21 @@
 // ==UserScript==
 // @name         ServiceNow Helper
 // @namespace    https://github.com/JohnyHCL/ServiceNowHelper/raw/master/ServiceNowHelper.user.js
-// @version      1.4
+// @version      1.5
 // @description  Adds a few features to the Service Now console.
 // @author       Jan Sobczak
 // @match        https://arcelormittalprod.service-now.com/*
 // @downloadURL  https://github.com/JohnyHCL/ServiceNowHelper/raw/master/ServiceNowHelper.user.js
 // @updateURL    https://github.com/JohnyHCL/ServiceNowHelper/raw/master/ServiceNowHelper.user.js
-// @grant        none
+// @grant        GM_setValue
+// @grant        GM_getValue
 // ==/UserScript==
 
 'use strict';
 
 var snMain = document.getElementById('dropzone1');
 var snInc = document.getElementById('incident.form_header');
-
+var bodyCount = document.body.childElementCount;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 if (snMain !== null){
@@ -179,6 +180,11 @@ if (snMain !== null){
         //            } else {
         window.open('https://arcelormittalprod.service-now.com' + target, '_blank');
         //            };
+        setTimeout(function(){
+            var RGpaste = GM_getValue('targetRG');
+            var text = "Transferring to " + RGpaste + "."
+            pasteAndPush(text);
+        }, 3000);
     };
 
     function pasteAndPush(text){
@@ -280,4 +286,17 @@ if (snMain !== null){
             };
         };
     };
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+} else if (bodyCount < 45){
+    console.log('AutoTransfer');
+    function RGcopy(){
+        var KBRG = document.getElementById('article').childNodes[4].childNodes[0].innerHTML.substring(0,24);
+        if(KBRG = "Transfer the incident to"){
+            var RG = document.getElementById('article').childNodes[4].childNodes[0].innerHTML.substring(27,200).slice(0,-3);
+            GM_setValue('targetRG', RG);
+            window.close();
+        };
+    };
+    RGcopy();
 };
+
