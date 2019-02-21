c023e7f7edd1aad3d18fa2cce314a2979ab61167
JohnyHCL <44863546+JohnyHCL@users.noreply.github.com>
2019-02-21 16:44:34 +0100

    Fix 1.9.3

--- ServiceNowHelper.user.js
+++ ServiceNowHelper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ServiceNow Helper
 // @namespace    https://github.com/JohnyHCL/ServiceNowHelper/raw/master/ServiceNowHelper.user.js
-// @version      1.9.2
+// @version      1.9.3
 // @description  Adds a few features to the Service Now console.
 // @author       Jan Sobczak
 // @match        https://arcelormittalprod.service-now.com/*
